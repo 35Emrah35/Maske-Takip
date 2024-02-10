1d434332e3508a032200a69972f70f012b265df0
secce <secce@DESKTOP-Q9OGLQ0>
2024-02-10 18:13:38 +0300

    Add project files.

---
+++ Workraund/Program.cs
@@ -0,0 +1,29 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+
+namespace Workraund
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person person1 = new Person();
+            person1.FirstName = "emrah";
+            person1.LastName = "cengiz";
+            person1.DateOfBirthYear = 1982;
+            person1.NationalIdentity = 123;
+
+                    
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person1);
+
+            Console.ReadLine();
+        }
+
+    }
+
+}
